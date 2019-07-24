4e42cc67e4c805532c0e986da9e2f96a39363a1a
KashafSyeda <50900045+KashafSyeda@users.noreply.github.com>
2019-07-24 14:00:12 +0500

    Update Quesation6.py

--- Quesation6.py
+++ Quesation6.py
@@ -10,24 +10,27 @@ Create a function that validates a password to conform to the following rules:
 
 '''
 
+while True:
+    a = list(input("enter new password: "))
+    if len(a) < 6 or len(a) > 24 :
+        print("min 6 max 24 digits allow")
+        continue
 
-a = list(input("enter new password: "))
-if len(a) < 6 or len(a) > 24 :
-    print("min 6 max 24 digits allow")
+    elif not(any(map(str.isupper, a))):
+        print("ERRor At least one uper case alphabet: ")
+        continue
 
-elif not(any(map(str.isupper, a))):
-    print("ERRor At least one uper case alphabet: ")
+    #elif any(map(str.isupper, a)):
+    #   print("error At least one lower case alphabet: ")
 
+    elif not any(i.isdigit() for i in a):
+        print("3RR0R at least add one digit ")  
+        continue
 
-#elif any(map(str.isupper, a)):
-#   print("error At least one lower case alphabet: ")
-
-elif not any(i.isdigit() for i in a):
-    print("3RR0R at least add one digit ")  
-
-s_c = ["!","@","#","$","%","^","&","*"]
-if not any(character in s_c for character in a) :
-    print("invalid: your password must contain !@#$%^&* any of these charactor: ")
-
-else:
-    print("PASSWORD has been created: ")
+    s_c = ["!","@","#","$","%","^","&","*"]
+    if not any(character in s_c for character in a) :
+        print("invalid: your password must contain !@#$%^&* any of these charactor: ")
+        continue    
+    else:
+        print("PASSWORD has been created: ")
+        break
